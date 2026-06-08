a1ccb05862d29b06170b90af1d7e255d4fec744d
Jakub Raczek <jakub.raczek@accenture.com>
2026-06-08 12:23:43 +0200

    Ensure jdbc_drivers directory is created before copying JDBC driver files

--- DBTestCompareGenerator/download-backup-and-jdbc-drivers.ps1
+++ DBTestCompareGenerator/download-backup-and-jdbc-drivers.ps1
@@ -8,5 +8,6 @@ Write-Output "Downloading sqljdbc drivers"
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2356503" -OutFile "./zip/sqljdbc_13.4.0.0_enu.zip"
 Write-Output "Unzipping sqljdbc drivers"
 Expand-Archive -LiteralPath './zip/sqljdbc_13.4.0.0_enu.zip' -DestinationPath "./zip/sqljdbc" -Force
+New-Item -Path './jdbc_drivers' -ItemType Directory -Force
 Copy-Item -Path "./zip/sqljdbc/sqljdbc_13.4/enu/jars/*"  -Destination "./jdbc_drivers" -Include "mssql-jdbc-*.jar"
 Remove-Item -Path  './zip/sqljdbc_13.4.0.0_enu.zip' -Force
